16a2e9441a68478656e8324093e4332016f859c2
bobriska <61400981+bobriska@users.noreply.github.com>
2022-12-04 11:34:33 +0200

    3.17

--- osa2/puhelinluettelo/src/components/PersonsForm.js
+++ osa2/puhelinluettelo/src/components/PersonsForm.js
@@ -15,9 +15,6 @@ const PersonsForm = ({newName, setNewName, newNumber, setNewNumber, persons, set
   
         const updateId = persons.filter(value => value.name === newName).map(value => value.id)[0]
         
-        console.log('new name ', newName)
-        console.log('update? ', updateId)
-
         if (updateId) {
             if (window.confirm(`${newName} is already in phonebook, do you want to update the number to ${newNumber}?`)) {
                 const personObject = {
